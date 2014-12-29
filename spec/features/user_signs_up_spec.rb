5e4afe6ac4b1e8ea282c8db733f3c18a6612f9b7
Nathan Burgess <nathan.burgess28@hotmail.com>
2014-12-28 22:10:07 -0500

    User can sign up

---
+++ spec/features/user_signs_up_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+feature "User sign up", %q(
+"As a Board Game Fanboy (or Fangirl)
+I want to sign up for the Game Collector application
+So that I can use all of its awesome features.
+
+Acceptance Criteria
+
+[ ] There is a link to 'Sign Up' on the homepage.
+[ ] If I fill in my first name, last name, email, password, and password confirmation correctly, I am greeted with a confirmation message that my account has been created.
+[ ] If the password and password confirmation fields do not match, I am given an error message.
+  [ ] If my email already exists in the database, I am given a message that tells me I have already registered.
+  [ ] If my email is not formatted correctly, I am given an error message."
+
+) do
+    scenario "user provides valid information" do
+      visit root_path
+      click_on "Sign Up"
+
+      fill_in "First Name", with: "Gene"
+      fill_in "Last Name", with: "Parmesean"
+      fill_in "Email", with: "gparmesean@privatateeyes.com"
+      fill_in "Password", with: "supersecret"
+      fill_in "Password confirmation", with: "supersecret"
+      click_on "Sign up"
+      expect(page).to have_content "Welcome! Your account has been created."
+
+    end
+
+
+
+
+
+end
