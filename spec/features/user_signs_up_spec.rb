96ea6dab7cf03113745208073efd19041419bafd
Nathan Burgess <nathan.burgess28@hotmail.com>
2014-12-28 22:13:36 -0500

    Add test for non-matching passwords

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -7,8 +7,8 @@ So that I can use all of its awesome features.
 
 Acceptance Criteria
 
-[ ] There is a link to 'Sign Up' on the homepage.
-[ ] If I fill in my first name, last name, email, password, and password confirmation correctly, I am greeted with a confirmation message that my account has been created.
+[x] There is a link to 'Sign Up' on the homepage.
+[x] If I fill in my first name, last name, email, password, and password confirmation correctly, I am greeted with a confirmation message that my account has been created.
 [ ] If the password and password confirmation fields do not match, I am given an error message.
   [ ] If my email already exists in the database, I am given a message that tells me I have already registered.
   [ ] If my email is not formatted correctly, I am given an error message."
@@ -25,6 +25,21 @@ Acceptance Criteria
       fill_in "Password confirmation", with: "supersecret"
       click_on "Sign up"
       expect(page).to have_content "Welcome! Your account has been created."
+
+    end
+
+
+    scenario "password fields do not match" do
+      visit root_path
+      click_on "Sign Up"
+
+      fill_in "First Name", with: "Gene"
+      fill_in "Last Name", with: "Parmesean"
+      fill_in "Email", with: "gparmesean@privatateeyes.com"
+      fill_in "Password", with: "supersecret"
+      fill_in "Password confirmation", with: "notmypassword"
+      click_on "Sign up"
+      expect(page).to have_content "Password confirmation doesn't match"
 
     end
 
